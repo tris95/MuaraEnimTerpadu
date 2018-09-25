48a8249082fbe1988ca09e96f6351d96e73dfd3e
Sutrisno <sutrisno.maret@gmail.com>
2018-09-25 16:41:31 +0700

    sliding 1.0

--- app/src/main/java/id/go/muaraenimkab/bappeda/muaraenimterpadu/fragments/DetailWisataFragment.java
+++ app/src/main/java/id/go/muaraenimkab/bappeda/muaraenimterpadu/fragments/DetailWisataFragment.java
@@ -2,17 +2,23 @@ package id.go.muaraenimkab.bappeda.muaraenimterpadu.fragments;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Objects;
 
 import id.go.muaraenimkab.bappeda.muaraenimterpadu.R;
 
 
 public class DetailWisataFragment extends Fragment {
-
+    Toolbar toolbar;
     public DetailWisataFragment() {
         // Required empty public constructor
     }
@@ -31,10 +37,21 @@ public class DetailWisataFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_detail_wisata, container, false);
+
+        toolbar = v.findViewById(R.id.toolbar);
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
+
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null) {
+            Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setDisplayShowHomeEnabled(true);
+            Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setTitle("");
+        }
+
         return v;
     }
 
